9dd6aa38ed1df045dba6ad9dafaa186a3703b097
Nícolas Aigner <nicolas.aigner@gmail.com>
2019-03-14 01:52:06 -0300

    add category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,12 +1,13 @@
 import {InMemoryDbService} from 'angular-in-memory-web-api';
+import {Category} from './pages/categories/shared/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
 
   createDb() {
-    const categories = [
+    const categories: Category[] = [
       { id: 1, name: 'Moradia', description: 'Pagamentos de Contas de Casa' },
-      { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
-      { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+      { id: 2, name: 'Saúde',   description: 'Plano de Saúde e Remédios' },
+      { id: 3, name: 'Lazer',   description: 'Cinema, parques, praia, etc' },
       { id: 4, name: 'Salário', description: 'Recebimento de Salário' },
       { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer' },
     ];

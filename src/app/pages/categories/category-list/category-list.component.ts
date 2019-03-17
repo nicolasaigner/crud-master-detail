09b81c86bf8c5ceede3d45a183d40bab098d6994
Nícolas Aigner <nicolas.aigner@gmail.com>
2019-03-17 15:10:24 -0300

    improving CategoryList and EntryList in BaseResourceListComponent

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,33 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {CategoryService} from '../shared/category.service';
 import {Category} from '../shared/category.model';
+import {BaseResourceListComponent} from '../../../shared/components/base-resource-list/base-resource-list.component';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
-  categories: Category[] = [];
-
-  constructor(private categoryService: CategoryService) { }
-
-  ngOnInit() {
-    this.categoryService.getAll().subscribe(
-      categories => this.categories = categories,
-        error => alert('Erro ao carregar a lista'));
+  constructor(private categoryService: CategoryService) {
+    super(categoryService);
   }
-
-  deleteCategory(category) {
-    const mustDelete = confirm(`Deseja realmente excluir o item ${category.name}?`);
-
-    if (mustDelete) {
-      this.categoryService.delete(category.id).subscribe(
-        () => this.categories = this.categories.filter(element => element != category),
-        () => alert(`Erro ao tentar excluir o item ${category.name}?`)
-      );
-    }
-  }
-
 }
--- src/app/shared/components/base-resource-list/base-resource-list.component.ts
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -10,7 +10,7 @@ export abstract class BaseResourceListComponent<T extends BaseResourceModel> imp
 
   ngOnInit() {
     this.resourceService.getAll().subscribe(
-      resources => this.resources = resources,
+      resources => this.resources = resources.sort((a, b) => b.id - a.id),
       error => alert('Erro ao carregar a lista'));
   }
 

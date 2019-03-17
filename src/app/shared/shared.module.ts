9a8db9c11cae549d23d5cc37eee62b6330c2e09c
Nícolas Aigner <nicolas.aigner@gmail.com>
2019-03-17 17:00:10 -0300

    addin breadCrumbCompoennt

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BreadCrumbComponent
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -11,6 +14,7 @@ import {ReactiveFormsModule} from '@angular/forms';
   exports: [
     CommonModule,
     ReactiveFormsModule,
+    BreadCrumbComponent
   ]
 })
 export class SharedModule { }

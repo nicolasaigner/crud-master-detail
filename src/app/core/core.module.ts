20f01402df0b82141e3ecff5a64b2ce5d5e48814
Nícolas Aigner <nicolas.aigner@gmail.com>
2019-03-17 02:52:05 -0300

    improving BaseResourceService and refactoring CategoryService

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from '../app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
+import {InMemoryDatabase} from '../in-memory-database';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   exports: [
     BrowserModule,

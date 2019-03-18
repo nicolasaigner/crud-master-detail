5170119cf3c142f8abf715dc45e7717f607ce22a
Nícolas Aigner <nicolas.aigner@gmail.com>
2019-03-17 21:31:54 -0300

    adding FormFieldErrorComponent to Forms

---
+++ src/app/shared/components/form-field-error/form-field-error.component.ts
@@ -0,0 +1,42 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+@Component({
+  selector: 'app-form-field-error',
+  template: `
+    <p class="text-danger">
+      {{errorMessage}}
+    </p>
+  `,
+  styleUrls: ['./form-field-error.component.css']
+})
+export class FormFieldErrorComponent implements OnInit {
+
+  @Input('form-control') formControl: FormControl;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public get errorMessage(): string | null {
+    if (this.mustShowErrorMessage()) {
+      return this.getErrorMessage();
+    } else {
+      return null;
+    }
+  }
+
+  private mustShowErrorMessage(): boolean {
+    return this.formControl.invalid && this.formControl.touched;
+  }
+
+  private getErrorMessage(): string | null {
+    if (this.formControl.errors.required)  {
+      return 'Dado obrigatório';
+    } else if (this.formControl.errors.minlength) {
+      const requiredLength = this.formControl.errors.minlength.requiredLength;
+      return `Deve ter no mínimo ${requiredLength} caracteres`;
+    }
+  }
+}
